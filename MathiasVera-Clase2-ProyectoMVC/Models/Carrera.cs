8cc7364e7c46b5a7ee3c15c8bf6d2b6eee73917a
MathiasVeraM <mathias.vera@udla.edu.ec>
2024-10-01 08:41:50 -0500

    Creacion de dos modelos para el proyecto y uso de anotaciones

---
+++ MathiasVera-Clase2-ProyectoMVC/Models/Carrera.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MathiasVera_Clase2_ProyectoMVC.Models
+{
+    public class Carrera
+    {
+        // Atributos de la Carrera que el estudiante curse
+        [Required]
+        [Range(1, 1000)] // Anotacion que permite que el Id de la carrera este entre 1 y 1000
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ MathiasVera-Clase2-ProyectoMVC/Models/EstudianteUDLA.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MathiasVera_Clase2_ProyectoMVC.Models
+{
+    //  Anotacion que afecta a toda la clase viene aqui ,  se puede excluir cosas
+    public class EstudianteUDLA
+    {
+        // Atributos de estudiante UDLA
+
+        [Required] // Anotacion que sirve para que el IDBanner sea requerido de llenar
+        public string IDBanner { get; set; }
+
+        [AllowNull] // Esta anotacion sirve para habilitar que el correo acepte nulos, igual el ?
+        [EmailAddress] // Esta anotacion sirve para validar que el usuario ingrese un correo valido en el formulario de vistas
+        public string? Correo {  get; set; }
+
+        [MaxLength(200)] // Permite que el nombre del estudiante tenga una longitud max de 100 caracteres
+        public string Nombre { get; set; }
+        public Carrera Carrera { get; set; }
+
+    }
+
+}
